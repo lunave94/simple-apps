9acffc0100a2c8247729166f44c7e79025de77f5
lunave94 <lunave94@gmail.com>
2025-10-29 13:00:34 +0700

    u

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
-const app = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 require("dotenv").config();
 
